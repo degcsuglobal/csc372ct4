9ac52abde27a4644463c1d1d8897f5369d7c6c56
Dan Grover <dan@standardbots.com>
2025-03-04 16:11:31 -0800

    forgot to add toString for sphere

--- src/csc372ct4/Sphere.java
+++ src/csc372ct4/Sphere.java
@@ -12,9 +12,12 @@ public class Sphere extends Shape {
 		return 4 * Math.PI * Math.pow(radius, 2);
 	}
 
-
 	public double volume() {
 		return (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+	}
+
+	public String toString() {
+		return String.format("<Sphere: radius = %f>", this.radius);
 	}
 
 }
